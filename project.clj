1290f8ed87e6649a7d06888a8f2e9f004e93e7f6
kevin.li <li_zhaoyu@msn.com>
2018-10-07 17:22:28 +0800

    clojure mail works now

--- project.clj
+++ project.clj
@@ -3,8 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [io.forward/clojure-mail "1.0.7"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [io.forward/clojure-mail "1.0.7"]
+                 [rodnaph/cail "0.8.8"]]
   :main ^:skip-aot mail.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/mail/core.clj
+++ src/mail/core.clj
@@ -1,22 +1,55 @@
 (ns mail.core
   (:require [clojure-mail.core :refer :all]
-            [clojure-mail.message :as message])
+            [clojure-mail.message :as message]
+            [cail.core :as cail]
+            [clojure.java.io :as io]
+            )
+  (:import [javax.mail.internet MimeUtility] )
   (:gen-class))
+
+
+(def qq-store (clojure-mail.core/store "imap.qq.com" "li_zhaoyu@qq.com" "xxsjjeacdnfrcahf"))
+
+(def my-inbox-messages (take 5 (all-messages qq-store "其他文件夹/发票")))
+
+(def first-message (first my-inbox-messages))
+
+(message/subject first-message)
+
+(defn pdf-map [msg-map]
+  (let [content-type (clojure.string/lower-case (:content-type msg-map))
+        mail-part-array (:body msg-map)
+        content-type-mixed "multipart/mixed"
+        content-type-pdf "application/pdf"
+        subject (:subject msg-map)
+        ]
+    (when (clojure.string/starts-with? content-type content-type-mixed)
+      (prn subject)
+      (for [part mail-part-array]
+        (let [part-body (:body part)
+              part-content-type (:content-type part)]
+          (if (and
+               (not (nil? part-body))
+               (clojure.string/starts-with?
+                (clojure.string/lower-case part-content-type)
+                content-type-pdf))
+            (io/copy part-body
+                     (io/file (str (java.util.UUID/randomUUID)
+                                   ".pdf")))
+            ))
+
+        )
+      ))
+  )
+
+(map pdf-map
+     (filter #(= (:multipart? %) true)
+             (map #(message/read-message % :fields [:body :multipart? :content-type :subject])
+                  (all-messages qq-store "其他文件夹/发票")
+                  )))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
-
-
-(defn qq-store [imap-url user-name password]
-  {:test }
-  (clojure-mail.core/store imap-url user-name password))
-
-(def qq-store (clojure-mail.core/store "imap.qq.com" "li_zhaoyu@qq.com" "xxsjjeacdnfrcahf"))
-
-(def my-inbox-messages (take 5 (all-messages qq-store "inbox")))
-
-(def first-message (first my-inbox-messages))
-
-(message/subject first-message)
